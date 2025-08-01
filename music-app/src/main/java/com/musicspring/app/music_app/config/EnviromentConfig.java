c86c8ad3aa27e216332424a9aede905efa1bb252
frannquir <franciscoquirogax@gmail.com>
2025-08-01 12:38:30 -0300

    env cfg fix

--- music-app/src/main/java/com/musicspring/app/music_app/config/EnviromentConfig.java
+++ music-app/src/main/java/com/musicspring/app/music_app/config/EnviromentConfig.java
@@ -1,14 +1,20 @@
 package com.musicspring.app.music_app.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Objects;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class EnviromentConfig {
-    static {
-        Dotenv dotenv = Dotenv.load();
+
+    @PostConstruct
+    public void init() {
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".")
+                .ignoreIfMissing()
+                .load();
 
         System.setProperty("jwt.secret", Objects.requireNonNull(dotenv.get("JWT_SECRET")));
         System.setProperty("spotify.client.id", Objects.requireNonNull(dotenv.get("SPOTIFY_CLIENT_ID")));
@@ -17,11 +23,9 @@ public class EnviromentConfig {
                 Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_ID")));
         System.setProperty("spring.security.oauth2.client.registration.google.client-secret",
                 Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_SECRET")));
-        System.setProperty("app.oauth2.redirect-uri", Objects.requireNonNull(
-                dotenv.get("APP_OAUTH2_REDIRECT_URI")));
-        System.setProperty("spring.datasource.url",Objects.requireNonNull(dotenv.get("DB_URL")));
+        System.setProperty("app.oauth2.redirect-uri", Objects.requireNonNull(dotenv.get("APP_OAUTH2_REDIRECT_URI")));
+        System.setProperty("spring.datasource.url", Objects.requireNonNull(dotenv.get("DB_URL")));
         System.setProperty("spring.datasource.username", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
         System.setProperty("spring.datasource.password", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
-
     }
 }
