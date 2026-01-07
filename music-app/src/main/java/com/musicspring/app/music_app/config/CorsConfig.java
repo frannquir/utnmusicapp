23e3de93964de8e6d3b0e46337f53f1bf0a3dd50
Pablo <pablosalompita@gmail.com>
2026-01-07 13:30:44 -0300

    Update CorsConfig.java

--- music-app/src/main/java/com/musicspring/app/music_app/config/CorsConfig.java
+++ music-app/src/main/java/com/musicspring/app/music_app/config/CorsConfig.java
@@ -17,13 +17,10 @@ public class CorsConfig implements WebMvcConfigurer {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "http://127.0.0.1:3000",
-                "http://localhost:4200",
-                "http://127.0.0.1:4200"
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "*"
         ));
-        
+
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
         ));
